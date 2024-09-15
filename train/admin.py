65f3d7b6f4600918f757329026b6312f0a413640
Stepan Lezhennikov <stepanlezennikov@gmail.com>
2024-09-15 23:04:50 +0300

    Models Train, Car, Seat added

--- train/admin.py
+++ train/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from train.models import Car, Train, Seat
+
+
+@admin.register(Seat)
+class SeatAdmin(admin.ModelAdmin):
+    fields = ['number', 'is_occupied', 'car']
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    fields = ['number', 'type', 'capacity']
+
+
+@admin.register(Train)
+class TrainAdmin(admin.ModelAdmin):
+    fields = ['number', 'type', 'cars']
+
+
--- train/models.py
+++ train/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
 
-# Create your models here.
+CarTypes = [
+    ("Плацкарт", "Плацкарт"),
+    ("Купе", "Купе"),
+    ("Сидячий", "Сидячий"),
+]
+
+TrainTypes = [
+    ("Скоростной", "Скоростной"),
+    ("Междугородний", "Междугородний"),
+    ("Пригородный", "Пригородный"),
+]
+
+
+class Train(models.Model):
+    number = models.IntegerField()
+    type = models.CharField(max_length=50, choices=TrainTypes)
+    cars = models.ManyToManyField('Car', related_name='train')
+
+    def __str__(self):
+        return f"Поезд {self.type} №{self.number}"
+
+
+class Car(models.Model):
+    number = models.IntegerField()
+    type = models.CharField(max_length=50, choices=CarTypes)
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f"Вагон {self.type} №{self.number}"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        for s in range(self.capacity):
+            Seat.objects.create(car=self, number=s + 1)
+
+
+class Seat(models.Model):
+    number = models.IntegerField()
+    is_occupied = models.BooleanField(default=False)
+    car = models.ForeignKey(Car, on_delete=models.CASCADE, related_name='seats', null=True)
+
+    def __str__(self):
+        return f"Место №{self.number} в вагоне {self.car.number}"
+
+
+@receiver(pre_delete, sender=Train)
+def delete_cars_with_train(sender, instance, **kwargs):
+    instance.cars.all().delete()
