dcfe26c0561e7ed68a6182d9abbd8a2720aafd95
Stepan Lezhennikov <stepanlezennikov@gmail.com>
2024-09-26 13:16:43 +0300

    Models Passenger, User added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Passenger(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    fathername = models.CharField(max_length=255, blank=True)
+    passport_number = models.CharField(max_length=10)
+    is_child = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.surname} {self.name} {self.fathername}"
+
+
+class User(AbstractUser):
+    passengers = models.ManyToManyField(Passenger, related_name='user')
