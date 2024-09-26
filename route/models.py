9339fa9cf474be2074362578e9ffbbd391985dde
Stepan Lezhennikov <stepanlezennikov@gmail.com>
2024-09-26 11:41:30 +0300

    Models Train, Car, Seat added

--- route/models.py
+++ route/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Station(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'Станция {self.name}'
+
+    class Meta:
+        verbose_name = "Станция"
+        verbose_name_plural = "Станции"
+
+
+class Route(models.Model):
+    from_station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='from_route')
+    to_station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='to_route')
+    departure_time = models.DateTimeField()
+    arrival_time = models.DateTimeField()
+
+    def __str__(self):
+        return f'Маршрут {self.from_station.name} -> {self.to_station.name}'
+
+    class Meta:
+        verbose_name = "Маршрут"
+        verbose_name_plural = "Маршруты"
+
+
+class Stop(models.Model):
+    route = models.ForeignKey(Route, on_delete=models.CASCADE, related_name='stops')
+    station = models.ForeignKey(Station, on_delete=models.CASCADE, related_name='stops')
+    arrival_time = models.DateTimeField()
+    departure_time = models.DateTimeField()
+
+    def __str__(self):
+        return f"Остановка: {self.station} | {self.route}"
+
+    class Meta:
+        verbose_name = "Остановка"
+        verbose_name_plural = "Остановки"
--- train/models.py
+++ train/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
+from django.db import connection
 
 CarTypes = [
     ("Плацкарт", "Плацкарт"),
@@ -16,12 +17,16 @@ TrainTypes = [
 
 
 class Train(models.Model):
-    number = models.IntegerField()
+    number = models.IntegerField(unique=True)
     type = models.CharField(max_length=50, choices=TrainTypes)
     cars = models.ManyToManyField('Car', related_name='train')
 
     def __str__(self):
         return f"Поезд {self.type} №{self.number}"
+
+    class Meta:
+        verbose_name = "Поезд"
+        verbose_name_plural = "Поезда"
 
 
 class Car(models.Model):
@@ -31,6 +36,10 @@ class Car(models.Model):
 
     def __str__(self):
         return f"Вагон {self.type} №{self.number}"
+
+    class Meta:
+        verbose_name = "Вагон"
+        verbose_name_plural = "Вагоны"
 
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
@@ -46,7 +55,30 @@ class Seat(models.Model):
     def __str__(self):
         return f"Место №{self.number} в вагоне {self.car.number}"
 
+    class Meta:
+        verbose_name = "Место"
+        verbose_name_plural = "Места"
+
 
 @receiver(pre_delete, sender=Train)
 def delete_cars_with_train(sender, instance, **kwargs):
-    instance.cars.all().delete()
+    with connection.cursor() as cursor:
+        cursor.execute("""
+                SELECT car_id FROM train_train_cars WHERE train_id = %s            
+        """, [instance.id])
+        car_ids = cursor.fetchall()
+        car_ids = [car_id[0] for car_id in car_ids]
+
+        for car_id in car_ids:
+            cursor.execute("""
+                DELETE FROM train_seat
+                WHERE car_id = %s   
+                """, [car_id])
+
+        cursor.execute("DELETE FROM train_train_cars WHERE train_id = %s", [instance.id])
+
+        for car_id in car_ids:
+            cursor.execute("""
+                DELETE FROM train_car
+                WHERE id = %s   
+                """, [car_id])
