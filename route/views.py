71245404678426ec7ff346f9e635f6bdb79acc94
Stepan Lezhennikov <stepanlezennikov@gmail.com>
2024-10-03 12:47:18 +0300

    Added counting of free seats in a train

--- route/views.py
+++ route/views.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 from django.db import connection
+from django.db.models import Count, Q
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.utils import timezone
@@ -20,19 +21,22 @@ def station_autocomplete(request):
             cursor.execute("SELECT name FROM route_station WHERE name ILIKE %s", [f'%{search_term}%'])
             stations = cursor.fetchall()
         station_names = [station[0] for station in stations]
-        print(station_names)
         return JsonResponse(station_names, safe=False)
-
     return JsonResponse([], safe=False)
 
 
 def search(request):
+
     start_time = timezone.datetime.fromisoformat(request.POST.get('travel_date'))
     end_time = start_time + timedelta(days=1)
     from_station = request.POST.get('from_station')
     to_station = request.POST.get('to_station')
-    routes = Route.objects.filter(from_station__name=from_station,
-                                  to_station__name=to_station,
-                                  departure_time__gt=start_time,
-                                  departure_time__lt=end_time)
+    routes = Route.objects.filter(
+        from_station__name=from_station,
+        to_station__name=to_station,
+        departure_time__gt=start_time,
+        departure_time__lt=end_time
+    ).select_related('train').annotate(
+        free_seats=Count('train__cars__seats', filter=Q(train__cars__seats__is_occupied=False))
+    )
     return render(request, 'route/result_search.html', context={'routes': routes})
